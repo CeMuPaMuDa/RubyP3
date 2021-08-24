19ecb54766976ff9c1f2c9488aa89a6dceb72c9c
CeMuPaMuDa <bepyc9i@gmail.com>
2021-08-25 00:36:43 +0300

    lesson7: 1-2

---
+++ lesson7_vera/2_colors.rb
@@ -0,0 +1,13 @@
+COLORS = {
+  red: 'красный',
+  orange: 'оранжевый',
+  yellow: 'желтый',
+  green: 'зеленый',
+  blue: 'голубой',
+  indigo: 'синий',
+  violet: 'фиолетовый'
+}.freeze
+puts "Хеш: #{COLORS}"
+puts "1) #{COLORS.values}"
+puts "2) #{COLORS.keys}"
+puts "3) #{COLORS.invert}"
