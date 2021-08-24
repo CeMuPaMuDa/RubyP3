1480af893fd337f197d60f33d171b4b8f20a50e6
CeMuPaMuDa <bepyc9i@gmail.com>
2021-08-24 15:37:48 +0300

    color

---
+++ lesson3_vera/2_colors.rb
@@ -0,0 +1,24 @@
+
+module Kernel
+  COLORS = {
+    red: 'красный',
+    orange: 'оранжевый',
+    yellow: 'желтый',
+    green: 'зеленый',
+    blue: 'голубой',
+    indigo: 'синий',
+    violet: 'фиолетовый'
+  }.freeze
+
+  def method_missing(COLORS)
+    @hash.fetch(COLORS, 'Чего тебе надобно, старче?')
+  end
+end
+
+
+# def respond_to_missing?(method, include_private = false)
+#   methods.include?(method) || super
+# end
+
+
+puts blue
